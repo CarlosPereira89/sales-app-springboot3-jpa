93a2b642561ddd97144a8e38e6d1923e4c967264
Carlos Pereira <carlos.programacao@gmail.com>
2022-12-07 20:40:17 -0300

    initial commit

---
+++ src/test/java/com/caps/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caps.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
